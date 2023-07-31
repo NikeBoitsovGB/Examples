24033e1a22bf4c32814a4ad21bf9b0af6eb42912
Narghinn <Narghinn@mail.ru>
2023-07-31 21:09:47 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ты хто? ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "хуй")
+{
+    Console.WriteLine("Шёл Хуй по хуёвой дорожке, увидел хуя в окошке");
+}
+else
+{
+    Console.Write("Иди нахуй, ");
+    Console.WriteLine(username);
+}
