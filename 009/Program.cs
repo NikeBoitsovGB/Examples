3fb5c4a369b74487443a1b46d20911abacce8f44
Narghinn <Narghinn@mail.ru>
2023-08-02 22:02:02 +0300

    009

---
+++ 009/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1234, 1451, 14515, 253, 63, 11, 66534, 13, 5532, 6667, 33};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while(index<n>)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
